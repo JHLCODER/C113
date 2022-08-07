829190c88ef2ff591515a84a992d4b3ad87ed906
JHLCODER <79390831+JHLCODER@users.noreply.github.com>
2022-08-07 12:13:25 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = 0;
 function preload(){
-
+mustache = loadImage(' https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
@@ -16,8 +18,10 @@ function setup(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log('nose x :' + results[0].pose.nose.x);
-        console.log('nose y :' + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 13;
+        noseY = results[0].pose.nose.y - 3;
+        console.log('nose x :' + noseX);
+        console.log('nose y :' + noseY);
     }
 }
 
@@ -27,6 +31,7 @@ function modelLoaded(){
 
 function draw(){
 image(video, 0, 0, 300, 300);
+image(mustache, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
